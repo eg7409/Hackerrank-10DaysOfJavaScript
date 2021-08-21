70f9f99b4376f91ccb5fda86905eb75ddaf4f949
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-21 12:52:22 +0530

    Commit

--- Day 2 Conditional Statements If-Else.js
+++ Day 2 Conditional Statements If-Else.js
@@ -1,4 +1,11 @@
+Objective
 
+In this challenge, we learn about if-else statements. Check out the attached tutorial for more details.
+
+Task
+
+Complete the getGrade(score) function in the editor. It has one parameter: an integer, , denoting the number of points Julia earned on an exam. 
+It must return the letter corresponding to her  according to the following rules:
 
 function getGrade(score) {
     let grade;
