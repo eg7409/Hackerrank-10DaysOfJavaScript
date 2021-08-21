f066f8bb50264e5e542a16e4c49c284d253f5b5b
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-21 12:53:04 +0530

    Commit

--- Day 2 Conditional Statements Switch.js
+++ Day 2 Conditional Statements Switch.js
@@ -1,3 +1,19 @@
+Objective
+
+In this challenge, we learn about switch statements. Check out the attached tutorial for more details.
+
+Task
+
+Complete the getLetter(s) function in the editor. It has one parameter: a string, 
+consisting of lowercase English alphabetic letters (i.e., a through z). It must return A, B, C, or D 
+depending on the following criteria:
+
+If the first character in string  is in the set , then return A.
+If the first character in string  is in the set , then return B.
+If the first character in string  is in the set , then return C.
+If the first character in string  is in the set , then return D.
+Hint: You can get the letter at some index  in  using the syntax s[i] or s.charAt(i).
+
 
 
 function getLetter(s) {
