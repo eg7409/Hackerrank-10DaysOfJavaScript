8063ba4631ce2fb3b2cde4f6139df07441234e49
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-21 12:53:51 +0530

    Update Day 2 Loops.js

--- Day 2 Loops.js
+++ Day 2 Loops.js
@@ -1,3 +1,23 @@
+Objective
+
+In this challenge, we practice looping over the characters of string. Check out the attached tutorial for more details.
+
+Task
+
+First, print each vowel in  on a new line. The English vowels are a, e, i, o, and u, and each vowel
+must be printed in the same order as it appeared in .
+Second, print each consonant (i.e., non-vowel) in  on a new line in the same order as it appeared in .
+Function Description
+
+Complete the vowelsAndConsonants function in the editor below.
+
+vowelsAndConsonants has the following parameters:
+
+string s: the string to process
+Prints
+
+Print each vowel of  in order on a new line, then print each consonant in order on a new line. Return nothing.
+
 function vowelsAndConsonants(s) {
     let vowels = ["a", "e", "i", "o", "u"];
 
@@ -7,8 +27,8 @@ function vowelsAndConsonants(s) {
     }
     
     for(let v of s) {
-        if(!vowels.includes(v))
+        if(!vowels.includes(v)){
             console.log(v);
-    
-}
+        } 
+     }
 }
